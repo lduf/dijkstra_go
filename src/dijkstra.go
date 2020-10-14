e5abd258b5d49bb3be3201c0e0a147a6f32d6655
lduf <65276047+lduf@users.noreply.github.com>
2020-10-14 17:38:12 +0200

    Ajout d'une fonction permettant de récupérer les voisins d'un point donné

--- src/dijkstra.go
+++ src/dijkstra.go
@@ -1,2 +1,29 @@
 // push test from goland
 package main
+
+import (
+	"fmt"
+)
+
+// On récupère la liste des noeuds pour la parser
+//pour chaque noeud -> je récupère la liste des voisins avec lesquels j'ai un lien
+
+func getNeighbors(point string, graph []elementGraph) []elementGraph {
+	//on travaille sur le point "point" appartenant la liste "graph"
+
+	//on parcourt notre slice graph et on regarde pour chaque élément si le point de départ est bien le point "point"
+	var neighbors []elementGraph
+	for _, elt := range graph {
+		if elt.from == point {
+			neighbors = append(neighbors, elt)
+		}
+	}
+	return neighbors
+}
+
+func main() {
+	graph, _ := fileToSlice()
+	neighbors := getNeighbors("A", graph)
+	fmt.Printf("%v \n ", neighbors)
+
+}

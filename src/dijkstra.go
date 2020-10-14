b1cd20cff0bede70e3f0812d15bd33986e82caf2
lduf <65276047+lduf@users.noreply.github.com>
2020-10-14 18:23:38 +0200

    Commentaires sur dijkstra

--- src/dijkstra.go
+++ src/dijkstra.go
@@ -1,4 +1,3 @@
-// push test from goland
 package main
 
 import (
@@ -7,33 +6,30 @@ import (
 
 // On récupère la liste des noeuds pour la parser
 //pour chaque noeud -> je récupère la liste des voisins avec lesquels j'ai un lien
-
 func getNeighbors(point string, graph []elementGraph) []elementGraph {
 	//on travaille sur le point "point" appartenant la liste "graph"
 
 	//on parcourt notre slice graph et on regarde pour chaque élément si le point de départ est bien le point "point"
 	var neighbors []elementGraph
-	for _, elt := range graph {
+	for _, elt := range graph { //elt est un élément du slice
 		if elt.from == point {
-			neighbors = append(neighbors, elt)
+			neighbors = append(neighbors, elt) //ajout de l'elt au slice
 		}
 	}
 	return neighbors
 }
+
+//Cette fonction permet de récupérer tous les voisins de tous les noeuds
+// La fonction retourne un map on peut donc appeler la liste des noeuds visins facilement
 func getAllNeighbors(graph []elementGraph, noeuds []string) map[string][]elementGraph {
 	allNeighbors := make(map[string][]elementGraph)
-	for _, noeud := range noeuds {
-		allNeighbors[noeud] = getNeighbors(noeud, graph)
+	for _, noeud := range noeuds { // parcours la liste des noeuds qui existe
+		allNeighbors[noeud] = getNeighbors(noeud, graph) // Ajout de la liste des voisins au map
 	}
 	return allNeighbors
 }
 
 func main() {
-	//graph, noeuds := fileToSlice()
-	//neighbors := getNeighbors("A", graph)
-	//fmt.Printf("%v \n ", neighbors)
-
 	//var allNeighbors map[string][]elementGraph
 	fmt.Printf("%v \n", getAllNeighbors(fileToSlice()))
-
 }

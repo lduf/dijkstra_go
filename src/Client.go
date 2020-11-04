52163dfbdf3b1eed63fa90f51f63b04a6e69777a
Lucas Dufour <lucasdufour@MacBook-Pro-de-Lucas.local>
2020-11-04 13:21:56 +0100

    Implementation des fichiers Client.go et Server.go
    Le sujet est maintenant completement traité (normalement)

--- src/Client.go
+++ src/Client.go
@@ -6,6 +6,7 @@ import (
 	"bufio"
 	"fmt"
 	"io"
+	"log"
 	"net"
 	"os"
 	"strconv"
@@ -13,6 +14,44 @@ import (
 	"time"
 )
 
+//Il faut ici que je me connecte à mon serveur
+//que j'extraie les datas de mon fichier pour les envoyer et prépare l'envoie des données  au serveur
+// ensuite j'envoie les datas au serveur
+// il traite les datas et me renvoie un graph que j'écris dans un out.txt
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+func getArgs() (int, string) {
+	// Vérifie qu'il y ai bien un argument
+	if len(os.Args) != 3 {
+		fmt.Println("Erreur : l'usage de Client.go nécessite l'appel suivant : go run Client.go <portNumber> <graph.txt>")
+		os.Exit(1)
+	} else {
+		//récupère le nom du fichier et vérifie que le fichier existe bien
+		portNumber, err := strconv.Atoi(os.Args[1])
+		if err != nil {
+			fmt.Printf("\"Vous devez utiliser le client ainsi : go run Client.go <portNumber>\n")
+			os.Exit(1)
+		} else {
+			filename := os.Args[2]
+			_, err := os.Stat(filename)
+			if os.IsNotExist(err) {
+				fmt.Printf("Erreur : le fichier %v n'existe pas, ou il fait référence à un dossier", filename)
+				os.Exit(1)
+			} else {
+				// Tout est ok, je retourne le nom du fichier pour la suite du script
+				return portNumber, filename
+			}
+		}
+		// Ne devrait jamais retourner
+	}
+	return -1, ""
+}
+
+/*
 func getPort() int {
 	//check for arg //os.Args provides access to raw command-line arguments. Note that the first value in this slice is the path to the program, and os.Args[1:] holds the arguments to the program.
 	if len(os.Args) != 2 {
@@ -30,35 +69,65 @@ func getPort() int {
 		}
 	}
 	return -1 //ne devrait jamais être atteint
-}
+}*/
 
 func main() {
-	port := getPort()
-	fmt.Printf("Dialing TCP server sur port :", port)
+	port, filename := getArgs()
 
+	//Connection au serveur
+	fmt.Printf("Dialing TCP server sur port : %d \n", port)
 	portString := fmt.Sprintf("127.0.0.1:%s", strconv.Itoa(port))
-	fmt.Printf(portString)
+	fmt.Printf(portString + "\n")
 	connection, err := net.Dial("tcp", portString)
 	if err != nil {
 		fmt.Printf("Connection echouée \n")
 		os.Exit(1)
-	} else {
+	} else { //Si ma connection marche
 		defer connection.Close()
 		reader := bufio.NewReader(connection)
 		fmt.Printf("Vous etes bien connectés \n")
-		//test d'envois
-		for i := 0; i < 10; i++ {
 
-			io.WriteString(connection, fmt.Sprintf("Yoooo %d\n", i))
-			resultString, err := reader.ReadString('\n')
-			if err != nil {
-				fmt.Printf("Lecture impossible")
-				os.Exit(1)
-			}
-			resultString = strings.TrimSuffix(resultString, "\n")
-			fmt.Printf("Le serveur à repondu : %v \n", resultString)
-			time.Sleep(1000 * time.Millisecond)
+		//on va envoyer le contenu de notre fichier
+
+		file, err := os.Open(filename)
+		checkError(err)
+		defer file.Close()
+		// on va parser notre fichier pour ajouter les lignes dans un slice
+		scanner := bufio.NewScanner(file)
+		for scanner.Scan() {
+			// On récupère la ligne du fichier et on l'envoie au serveur
+			txt := scanner.Text()
+			io.WriteString(connection, txt+"\n") ///Ici on a l'envoie des datas
+			fmt.Printf("Envoie de : %v \n", txt)
 		}
+		//check si on a une erreur avec le scanner
+		if err := scanner.Err(); err != nil {
+			os.Exit(1)
+		}
+		//Après avoir tout envoyé on récupère la réponse du serveur
+		outfile := fmt.Sprintf("out/out_%v.txt", time.Now().Unix())
+		for {
+			resultString, err := reader.ReadString('\n') //Là on attend la réponse du serveur
+
+			if err != nil {
+				fmt.Printf("Le serveur ne renvoie aucune donnée \n")
+				break
+			}
+
+			resultString = strings.TrimSuffix(resultString, "\n")
+			fmt.Printf("Réponse du serveur : %v \n ", resultString)
+
+			f, err := os.OpenFile(outfile,
+				os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+			if err != nil {
+				log.Println(err)
+			}
+			defer f.Close()
+			if _, err := f.WriteString(resultString + "\n"); err != nil {
+				log.Println(err)
+			}
+		}
+		fmt.Printf("L'analyse de dijkstra est contenu dans : %v \n", outfile)
 
 	}
 
--- src/Server.go
+++ src/Server.go
@@ -6,10 +6,175 @@ import (
 	"io"
 	"net"
 	"os"
+	"sort"
 	"strconv"
 	"strings"
+	"sync"
 )
 
+// DEBUT DE DIJKSTRA
+
+// On récupère la liste des noeuds pour la parser
+//pour chaque noeud -> je récupère la liste des voisins avec lesquels j'ai un lien pour un noeud donné
+func getNeighbors(point string, graph []elementGraph) []elementGraph {
+	//on travaille sur le point "point" appartenant la liste "graph"
+
+	//on parcourt notre slice graph et on regarde pour chaque élément si le point de départ est bien le point "point"
+	var neighbors []elementGraph
+	for _, elt := range graph { //elt est un élément du slice
+		if elt.from == point {
+			neighbors = append(neighbors, elt) //ajout de l'elt au slice
+		}
+	}
+	return neighbors
+}
+
+//Cette fonction permet de récupérer tous les voisins de tous les noeuds
+// La fonction retourne un map on peut donc appeler la liste des noeuds visins facilement
+func getAllNeighbors(graph []elementGraph, noeuds []string) map[string][]elementGraph {
+	allNeighbors := make(map[string][]elementGraph)
+	for _, noeud := range noeuds { // parcours la liste des noeuds qui existe
+		allNeighbors[noeud] = getNeighbors(noeud, graph) // Ajout de la liste des voisins au map
+	}
+	return allNeighbors
+}
+
+//permet d'obtenir le poid le plus petit pour un graph donné
+func getMin(graphPart []chemin) chemin {
+	minKey := 0
+	minValue := graphPart[0].weight
+	for k, elt := range graphPart {
+		if elt.weight < minValue {
+			minValue = elt.weight
+			minKey = k
+		}
+	}
+	return graphPart[minKey]
+}
+
+// Cette fonction permet de récupérer la valeur mini contenue dans notre tableau dijkstra
+func getMinDijk(dijksTAB map[string][]chemin, deadPoints map[string]int) (string, int) {
+	min := -1 // Attention on considère ici que les poids ne peuvent que être positifs
+	minPoint := ""
+	minKey := 0
+	for point, i := range dijksTAB { //(i pour iterration) -> c'est un slice de chemin
+		////fmt.Printf("##### DEBUG min:   Je suis au point %v \n", point)
+		if _, ok := deadPoints[point]; !ok {
+			////fmt.Printf("##### DEBUG min: %v n'est pas un point mort \n", point)
+			for k, chm := range i { // k est l'indice du chemin chm
+				if min != -1 && chm.weight < min { // si min est défini et que le poids du chemin est inférieur au min
+					min = chm.weight
+					minPoint = point
+					minKey = k
+				}
+				if min == -1 { //si min n'a pas été défini
+					min = chm.weight
+					minPoint = point
+					minKey = k
+				}
+				////fmt.Printf("##### DEBUG min: Pour l'instant le chemin le plus court est : %v depuis le point %v \n", dijksTAB[minPoint][minKey], minPoint)
+			}
+		}
+	}
+	return minPoint, minKey
+}
+
+type chemin struct {
+	from   string
+	weight int
+}
+
+func reverse(s []string) []string { // Permet de reverse un slice de string
+	for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
+		s[i], s[j] = s[j], s[i]
+	}
+	return s
+}
+
+/*
+func getDijkstra1to1(from string, to string) ([]string, int) {
+	ways, distances := getDijkstra(from)
+	return ways[to], distances[to]
+}*/
+
+// permet de retourner le Dijkstra considerant un point de départ (from)
+
+func getDijkstra(from string, wg *sync.WaitGroup, graph []elementGraph, noeuds []string) (map[string][]string, map[string]int) {
+	defer wg.Done() // on vire notre waitgroup
+	//initialisation des variables
+	ways := make(map[string][]string) //va contenir tous les chemins
+	distances := make(map[string]int) //distance totale parcourue
+	// À la main on utilise un tableau à 2 entrées : A | B | C | ... | Z (nom des noeuds) et le nombre de "tour". On itère à chaque tour pour trouver la distance la plus courte.
+	dijksTAB := make(map[string][]chemin) // contient en gros tout le travail
+	deadPoints := make(map[string]int)    //nom des noeuds par lesquels on ne peut pas repasser
+
+	//Étape 1 : on créé notre tableau dans lequel on appliquera l'algo
+	//graph, noeuds := fileToSlice()              //On récupère ici le graph sur lequel on va travailler et la liste des noeuds présents dans le graph
+	neighbors := getAllNeighbors(graph, noeuds) //voisins de tous les noeuds
+
+	dijksTAB[from] = append(dijksTAB[from], chemin{from, 0}) //initialisation du tableau
+	//deadPoints[from] = 0
+
+	//on récupère la distance la plus courte
+	for i := 0; i < len(noeuds); i += 1 { //On parcourt autant de fois qu'il y a de noeud
+		//fmt.Printf("\n \n \n### DEBUG : Nouveau tour \n")
+		pt, k := getMinDijk(dijksTAB, deadPoints) //je récupère le point et la clé contenant la distance la plus courte
+		smallestWay := dijksTAB[pt][k]            // Ici j'ai le chemin le plus court
+		//fmt.Printf("### DEBUG : J'ai récupéré le chemin le plus court : lettre %v poid : %d \n", pt, smallestWay.weight)
+		deadPoints[pt] = i // J'ajoute au point mort le point contenu dans le chemin le plus court
+		//fmt.Printf("### DEBUG : Ajout de %v aux points morts avecc l'indice : %d \n", pt, i)
+		//fmt.Printf("### DEBUG : Parcourt des voisins de lettre %v  \n", pt)
+		for _, direction := range neighbors[pt] { //pour tous les voisins du point du chemin le plus court
+			//fmt.Printf("### DEBUG :  %v à pour voisin %v \n", pt, direction.to)
+			if _, ok := deadPoints[direction.to]; !ok { // si la direction du point vers lequel je vais n'est pas dans la liste des points morts alors
+				//fmt.Printf("### DEBUG :  %v n'est pas un point mort \n", direction.to)
+				dijksTAB[direction.to] = append(dijksTAB[direction.to], chemin{direction.from, direction.weight + smallestWay.weight}) //De mon point, il existe un nouveau chemin vers le point direction.to de poid total := plus petit
+				//fmt.Printf("### DEBUG :  Je peux aller en %v depuis %v. Cette route à un poid de %d menant à un poid total de %d \n", direction.to, direction.from, direction.weight, direction.weight+smallestWay.weight)
+			}
+		}
+		//fmt.Printf("### DEBUG : voici à quoi ressemble mon Dijsktra %v \n \n \n", dijksTAB)
+	}
+	//À ce niveau, on a récupéré le tableau dijkstra pour à partir de la lettre from.
+	// Il ne reste plus qu'à remonter le tableau pour retourner à from !
+	for _, noeud := range noeuds {
+		ways[noeud] = append(ways[noeud], noeud) //Ajout du noeud d'arrivé
+		n := noeud
+		for getMin(dijksTAB[n]).from != from {
+			ways[noeud] = append(ways[noeud], getMin(dijksTAB[n]).from)
+			n = getMin(dijksTAB[n]).from
+		}
+		ways[noeud] = append(ways[noeud], from) //Ajout du noeud de départ
+		distances[noeud] = getMin(dijksTAB[noeud]).weight
+		//Je reverse mon way
+		ways[noeud] = reverse(ways[noeud])
+	}
+
+	return ways, distances
+}
+func Dijkstra(graph []elementGraph, noeuds []string) (map[string]map[string][]string, map[string]map[string]int) {
+	var wg sync.WaitGroup // Waitgroup
+	//_, noeuds := fileToSlice()
+	dijk := make(map[string]map[string][]string)
+	distances := make(map[string]map[string]int)
+	for _, noeud := range noeuds {
+		var ways map[string][]string
+		var dists map[string]int
+
+		wg.Add(1)
+		go func() {
+			ways, dists = getDijkstra(noeud, &wg, graph, noeuds)
+		}()
+		wg.Wait()
+		dijk[noeud] = ways
+		distances[noeud] = dists
+	}
+
+	return dijk, distances
+}
+
+//fin de Dijkstra
+
+// permet de récupérer le port sur lequel le serveur est créé
 func getPortS() int {
 	//check for arg //os.Args provides access to raw command-line arguments. Note that the first value in this slice is the path to the program, and os.Args[1:] holds the arguments to the program.
 	if len(os.Args) != 2 {
@@ -20,7 +185,7 @@ func getPortS() int {
 		fmt.Printf("Vous avez indiqué le port :\n", os.Args[1])
 		portNumber, err := strconv.Atoi(os.Args[1])
 		if err != nil {
-			fmt.Printf("\"Vous devez utiliser le server ainsi : go run Server.go <portNumber>\n")
+			fmt.Printf("Vous devez utiliser le server ainsi : go run Server.go <portNumber>\n")
 			os.Exit(1)
 		} else {
 			return portNumber
@@ -29,13 +194,39 @@ func getPortS() int {
 	return -1 //ne devrait jamais être atteint
 }
 
+type elementGraph struct {
+	from   string
+	to     string
+	weight int
+}
+
+//Cette fonction permet de mettre les caractères d'une liste en majuscule
+func listToUpper(list []string) {
+	for key, elt := range list {
+		list[key] = strings.ToUpper(elt)
+	}
+}
+
+//Cette fonction premet de retirer les valeurs dupliquées dans un slice
+func unique(slice []string) []string {
+	keys := make(map[string]bool)
+	list := []string{}
+	for _, entry := range slice {
+		if _, value := keys[entry]; !value {
+			keys[entry] = true
+			list = append(list, entry)
+		}
+	}
+	return list
+}
+
 func main() {
-	port := getPortS()
-	fmt.Printf("Creation d'un server TCP local sur le port :\n", port)
+	port := getPortS() //récup le port
+	fmt.Printf("Creation d'un server TCP local sur le port : %d \n", port)
 	//creation du portString avec le bon format pour écouter
 	portString := fmt.Sprintf(":%s", strconv.Itoa(port))
 
-	ecoute, err := net.Listen("tcp", portString)
+	ecoute, err := net.Listen("tcp", portString) //création de l'écoute du serveur
 	if err != nil {
 		fmt.Printf("L'instance ecoute n'a pas pu être crée\n")
 		panic(err)
@@ -44,9 +235,9 @@ func main() {
 
 	ct := 1
 
-	for {
+	for { //Tout le temps on attend les connections
 		fmt.Printf("Acceptation de la prochaine connection\n")
-		connection, errc := ecoute.Accept()
+		connection, errc := ecoute.Accept() //on accepte la connecttion
 
 		if errc != nil {
 			fmt.Printf("Erreur lors de l'acceptation de la prochaine connection")
@@ -54,7 +245,7 @@ func main() {
 		}
 		//si nous sommes ici il n'y a pas d'erreur et panic ne s'est pas executée
 
-		go handleConnection(connection, ct)
+		go handleConnection(connection, ct) //si la connection avec le client est OK
 		ct += 1
 	}
 }
@@ -64,19 +255,47 @@ func handleConnection(connect net.Conn, ct int) {
 	defer connect.Close()
 	connectReader := bufio.NewReader(connect)
 
+	var slice []elementGraph
+	var noeuds []string
+
 	for {
-		inputLine, err := connectReader.ReadString('\n')
+		inputLine, err := connectReader.ReadString('\n') //on récupère la ligne envoyée par le client
 		if err != nil {
 			fmt.Printf("Error but no panic")
 			fmt.Printf("Error :\n", err.Error())
 			break
 		}
-
 		inputLine = strings.TrimSuffix(inputLine, "\n")
-		fmt.Printf("RCV %d %v \n", ct, inputLine)
-		splitLine := strings.Split(inputLine, " ")
-		returnedString := splitLine[len(splitLine)-1]
-		fmt.Printf("SND %d %v \n", ct, returnedString)
-		io.WriteString(connect, fmt.Sprintf("%s\n", returnedString))
+		fmt.Printf("%v \n", inputLine)
+		splitted := strings.Split(inputLine, " ")
+		if splitted[2] != "." {
+			noeuds = append(noeuds, strings.ToUpper(splitted[0]), strings.ToUpper(splitted[1]))
+			// Je convertis mon poids en entier pcq il était stocké comme un int
+			weight, _ := strconv.Atoi(splitted[2])
+			// J'ajoute à mon slice un elementGraph
+			slice = append(slice, elementGraph{splitted[0], splitted[1], weight})
+		} else {
+			break
+		}
+
+		/*
+			fmt.Printf("RCV %d %v \n", ct, inputLine)
+			splitLine := strings.Split(inputLine, " ")
+			returnedString := splitLine[len(splitLine)-1]
+			fmt.Printf("SND %d %v \n", ct, returnedString)
+			io.WriteString(connect, fmt.Sprintf("%s\n", returnedString))*/
+	}
+	listToUpper(noeuds)
+	noeuds = unique(noeuds)
+	sort.Strings(noeuds)
+
+	ways, distances := Dijkstra(slice, noeuds)
+
+	for letter, graph := range ways {
+		for l, way := range graph {
+			out := fmt.Sprintf("%v %v %v %v \n", letter, l, way, distances[letter][l])
+			fmt.Printf("Envoie de : %v", out)
+			io.WriteString(connect, fmt.Sprintf("%s", out))
+		}
 	}
 }
